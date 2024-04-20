850ad61586170145f229f25558893a9f38db3bcb
Karan suthar <kan.suthar666@gmail.com>
2024-04-20 19:32:02 +0530

    solved leetcode problem 83

---
+++ Q83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,42 @@
+public class Q83_Remove_Duplicates_from_Sorted_List {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null)
+                return head;
+            ListNode currentNode = head;
+            int currentVal = head.val;
+            while (true) {
+                if (currentNode.next == null)
+                    break;
+
+                if (currentNode.next.val == currentVal) {
+                    currentNode.next = currentNode.next.next;
+                    continue;
+                }
+                if (currentNode.next == null)
+                    break;
+                currentNode = currentNode.next;
+                currentVal = currentNode.val;
+            }
+            return head;
+
+        }
+    }
+}
