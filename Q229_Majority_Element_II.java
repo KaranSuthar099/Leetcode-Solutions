8656c88f80a90cb9c1edbfcb6c3ab43436fe2ce0
Karan suthar <kan.suthar666@gmail.com>
2024-04-29 23:31:40 +0530

    solved leetcode problem 229

--- Q229_Majority_Element_II.java
+++ Q229_Majority_Element_II.java
@@ -1,23 +1,46 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Q229_Majority_Element_II {
     public static List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> frequency = new HashMap<>();
-        for (int i: nums){
-            if (frequency.containsKey(i)) frequency.put(i, frequency.get(i)+1);
-            else frequency.put(i, 1);
-        }
         List<Integer> retArray = new ArrayList<>(2);
-        int index = 0;
-        for (int i: frequency.keySet()){
-            if (index == 2) break;
-            if (frequency.get(i) > nums.length/3){
-                retArray.add(i);
-                index++;
+        if (nums.length == 1) {
+            retArray.add(nums[0]);
+            return retArray;
+        }
+
+        int count1 = 0;
+        int element1 = -10909090;
+        int count2 = 0;
+        int element2 = -1090909;
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 == 0 && nums[i] != element2) {
+                element1 = nums[i];
+                count1++;
+            } else if (count2 == 0 && nums[i] != element1) {
+                element2 = nums[i];
+                count2++;
+            } else if (nums[i] == element1) count1++;
+            else if (nums[i] == element2) count2++;
+            else {
+                count1--;
+                count2--;
             }
         }
+
+        count1 = 0;
+        count2 = 0;
+        for (int i : nums) {
+            if (i == element1) count1++;
+            if (i == element2) count2++;
+        }
+
+        int threshold = nums.length / 3;
+        if (count1 > threshold)
+            retArray.add(element1);
+        if (count2 > threshold)
+            retArray.add(element2);
+
         return retArray;
 
     }
