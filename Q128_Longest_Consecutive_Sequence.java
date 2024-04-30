740918f6ff27b8ffc219fbaf5859fd869e70e85c
Karan suthar <kan.suthar666@gmail.com>
2024-04-30 15:36:39 +0530

    solved leetcode problem 128

--- Q128_Longest_Consecutive_Sequence.java
+++ Q128_Longest_Consecutive_Sequence.java
@@ -1,27 +1,48 @@
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+//import java.util.HashSet;
+//import java.util.Set;
 
 public class Q128_Longest_Consecutive_Sequence {
     public static int longestConsecutive(int[] nums) {
         //return the length of longest consecutive array
+
+        if (nums.length == 0) return 0;
+
         Arrays.sort(nums);
-
-        Set<Integer> setChar = new HashSet<>();
-        for (int i : nums) {
-            setChar.add(i);
-        }
-
-        int longestConsecutive = 0;
-        for (int number : nums) {
-            int consecutiveCount = 1;
-            while (setChar.contains(number + 1)) {
-                setChar.remove(number+1);
-                consecutiveCount++;
-                number++;
+        int longestSequence = 0;
+        int currentSequence = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] == nums[i]) ;
+            else if (nums[i + 1] == nums[i] + 1)
+                currentSequence++;
+            else {
+                if (currentSequence > longestSequence) longestSequence = currentSequence;
+                currentSequence = 1;
             }
-            if (consecutiveCount > longestConsecutive) longestConsecutive = consecutiveCount;
         }
-        return longestConsecutive;
+        if (currentSequence > longestSequence) longestSequence = currentSequence;
+        return longestSequence;
     }
+
+//    public static int longestConsecutive(int[] nums) {
+//        //return the length of longest consecutive array
+//        Arrays.sort(nums);
+//
+//        Set<Integer> setChar = new HashSet<>();
+//        for (int i : nums) {
+//            setChar.add(i);
+//        }
+//
+//        int longestConsecutive = 0;
+//        for (int number : nums) {
+//            int consecutiveCount = 1;
+//            while (setChar.contains(number + 1)) {
+//                setChar.remove(number+1);
+//                consecutiveCount++;
+//                number++;
+//            }
+//            if (consecutiveCount > longestConsecutive) longestConsecutive = consecutiveCount;
+//        }
+//        return longestConsecutive;
+//    }
 }
