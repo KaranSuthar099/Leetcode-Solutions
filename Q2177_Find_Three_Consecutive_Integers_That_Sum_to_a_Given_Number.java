fca8fc245173c13433b9f4d762458d696eb4ea37
Karan suthar <kan.suthar666@gmail.com>
2024-04-30 16:22:15 +0530

    solved leetcode problem 2177

---
+++ Q2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number.java
@@ -0,0 +1,8 @@
+public class Q2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number {
+    public long[] sumOfThree(long num) {
+        if (num % 3 == 0) {
+            return new long[]{num / 3 - 1, num / 3, num/3 + 1};
+        }else return new long[]{};
+    }
+
+}
