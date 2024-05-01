5f3c4d05a9a66bf7880f8c219cf37364d5d8b04f
Karan suthar <kan.suthar666@gmail.com>
2024-05-01 17:09:43 +0530

    solved leetcode problem 1

---
+++ Q1_Two_Sum.java
@@ -0,0 +1,14 @@
+public class Q1_Two_Sum {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=1;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if(nums[j-i] + nums[j]==target){
+                    return new int[]{j-i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+}
