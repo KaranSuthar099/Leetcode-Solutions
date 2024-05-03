8723b6351699a937edf6d37e7984c905b4f59da6
Karan suthar <kan.suthar666@gmail.com>
2024-05-03 22:26:04 +0530

    solved leetcode problem 26

--- Q26_Remove_Duplicates_from_Sorted_Array.java
+++ Q26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,17 +1,11 @@
-import java.util.HashSet;
-
 public class Q26_Remove_Duplicates_from_Sorted_Array {
     public static int removeDuplicates(int[] nums) {
-        HashSet<Integer> elements = new HashSet<>();
-
-        int[] retNums = new int[nums.length];
-        int counter = 0;
-        for (int i:nums){
-            if (!elements.contains(i)){
-                nums[counter++] = i;
-                elements.add(i);
-            }
+        // now solving using two pointer
+        int current = 0;
+        for(int i = 1; i<nums.length; i++){
+            if (nums[i] != nums[current])
+                nums[++current] = nums[i];
         }
-        return elements.size();
+        return ++current;
     }
 }
