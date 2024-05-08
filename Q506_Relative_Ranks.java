5cdd1001c6250758558a187b17967a671739452c
Karan suthar <kan.suthar666@gmail.com>
2024-05-08 22:01:26 +0530

    solved leetcode problem 506

--- Q506_Relative_Ranks.java
+++ Q506_Relative_Ranks.java
@@ -1,28 +1,25 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Q506_Relative_Ranks {
-
     public static String[] findRelativeRanks(int[] score) {
         int[] s = Arrays.stream(score).sorted().toArray();
 
-        int length = s.length;
         HashMap<Integer, String> map = new HashMap<>();
-        String[] retArray = new String[length];
+        String[] retArray = new String[s.length];
 
         int rank = 1;
-        for ( int i = s.length-1; i>=0; i--){
+        for (int i = s.length - 1; i >= 0; i--) {
             if (rank > 3) map.put(s[i], String.valueOf(rank));
-            else if (rank == 3) map.put(s[length-3], "Bronze Medal");
-            else if (rank == 2) map.put(s[length-2], "Silver Medal");
-            else if (rank == 1) map.put(s[length-1], "Gold Medal");
+            else if (rank == 3) map.put(s[s.length - 3], "Bronze Medal");
+            else if (rank == 2) map.put(s[s.length - 2], "Silver Medal");
+            else if (rank == 1) map.put(s[s.length - 1], "Gold Medal");
             rank++;
 
         }
-        int index = 0;
-        for(int value : score){
-            retArray[index++] = map.get(value);
+        rank = 0;
+        for (int value : score) {
+            retArray[rank++] = map.get(value);
         }
         return retArray;
     }
@@ -31,5 +28,4 @@ public class Q506_Relative_Ranks {
         int[] score = {1, 2, 3};
         System.out.println(Arrays.toString(findRelativeRanks(score)));
     }
-
 }
