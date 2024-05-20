9f418188bcb5d3dea77f2e0771155afeffcccb94
Karan suthar <kan.suthar666@gmail.com>
2024-05-20 23:39:50 +0530

    solved leetcode problem 35

--- Q35_Search_Insert_Position.java
+++ Q35_Search_Insert_Position.java
@@ -1,22 +1,19 @@
 public class Q35_Search_Insert_Position {
     public static int searchInsert(int[] nums, int target) {
+        // simple binary search question
         if (target > nums[nums.length-1]) return nums.length;
         if (target < nums[0]) return 0;
 
         int left = 0;
         int right = nums.length -1;
-        int middle = -1;
-        while (right - left != 1){
-            middle = (right-left) /2 + left;
+        while (left<=right){
+            int middle = (right-left) /2 + left;
             if (nums[middle] < target)
-                left = middle;
+                left = middle+1;
             else if ( nums[middle] > target)
-                right = middle;
-            else if (nums[middle] == target) return middle;
+                right = middle+1;
+            else return middle;
         }
-
-        if ( nums[left] == target) return left;
-
         return right;
     }
 }
