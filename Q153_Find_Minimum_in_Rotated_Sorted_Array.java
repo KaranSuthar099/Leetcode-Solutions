e4b385ea332f5be768a06dc5c1a2fa7076105efe
Karan suthar <kan.suthar666@gmail.com>
2024-05-23 19:12:03 +0530

    solved leetcode problem 153

--- Q153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ Q153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -1,27 +1,24 @@
 public class Q153_Find_Minimum_in_Rotated_Sorted_Array {
 
     public static void main(String[] args) {
-        int[] numbs = { 6, 7, 8, 9, 1, 2, 3, 4, 5};
+        int[] numbs = {6, 7, 8, 9, 1, 2, 3, 4, 5};
         System.out.println(findMin(numbs));
     }
 
 
     public static int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
+        if (nums[0] < nums[nums.length - 1]) return nums[0];
 
-        if (nums[left] < nums[right])
-            return nums[0];
+        int start = 0;
+        int end = nums.length - 1;
 
-        while (left != right && right -1 != left){
-            int middle = left + (right - left)/2;
-            if (nums[right] > nums[middle])
-                right = middle;
-            else
-                left = middle;
+        while (start < end) {
+            int middle = start + (end - start) / 2;
+
+            if (nums[middle] >= nums[0]) start = middle + 1;
+            else end = middle;
         }
-
-        return Math.min(nums[left], nums[right]);
+        return nums[start];
     }
 
 }
