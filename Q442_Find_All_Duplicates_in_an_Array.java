fe8e8637cea37c88256ab45d1b35a857d83d6659
Karan suthar <kan.suthar666@gmail.com>
2024-05-26 23:12:58 +0530

    solved leetcode problem 442

--- Q442_Find_All_Duplicates_in_an_Array.java
+++ Q442_Find_All_Duplicates_in_an_Array.java
@@ -1,26 +1,24 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class Q442_Find_All_Duplicates_in_an_Array {
 
     public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] nums = {10, 2, 5, 10, 9, 1, 1, 4, 3, 7};
         System.out.println(findDuplicates(nums));
+        // [7,10]
     }
 
     public static List<Integer> findDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
         ArrayList<Integer> retArray = new ArrayList<>();
 
-        // copying the list in set
-        for (int i : nums) {
-            if (set.contains(i))
-                retArray.add(i);
-            else
-                set.add(i);
-        }
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
 
+            if (nums[index] > 0) nums[index] *= -1;
+            else retArray.add(index+1);
+
+        }
         return retArray;
     }
 }
