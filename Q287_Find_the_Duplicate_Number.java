c12db1334551e76316a4e273842413b6477c4135
Karan suthar <kan.suthar666@gmail.com>
2024-05-28 14:09:05 +0530

    solved leetcode problem 287

--- Q287_Find_the_Duplicate_Number.java
+++ Q287_Find_the_Duplicate_Number.java
@@ -1,20 +1,27 @@
 public class Q287_Find_the_Duplicate_Number {
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 1, 4, 2};
+        int[] nums = {3, 1, 3, 4, 2};
         System.out.println(findDuplicate(nums));
     }
 
     public static int findDuplicate(int[] nums) {
-        // using boolean array
-        int[] array = new int[nums.length];
-        for (int num : nums) {
-            array[num - 1]++;
+        // using tortoise and hare
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast){
+                break;
+            }
         }
-
-        for (int i = 0; i<array.length; i++){
-            if (array[i] > 1) return i+1;
+        // slow and fast are in the cycle
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return fast;
     }
 }
