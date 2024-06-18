a85e671f31b510ab1e9f44f05f20e7c91f309065
Karan suthar <kan.suthar666@gmail.com>
2024-06-19 00:12:51 +0530

    solved leetcode problem 49

--- Q49_Group_Anagrams.java
+++ Q49_Group_Anagrams.java
@@ -7,25 +7,15 @@ public class Q49_Group_Anagrams {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-        for (String s: strs){
-            int[] keyArray = new int[26];
-            for (char c: s.toCharArray()){
-                keyArray[c-97]++;
-            }
-
-            // convert this to Stringbuilder
+        for (String s : strs) {
+            char[] keyArray = s.toCharArray();
+            Arrays.sort(keyArray);
             String key = Arrays.toString(keyArray);
 
-
-            if (map.containsKey(key)){
-                List<String> list = map.get(key);
-                list.add(s);
-                map.put(key, list);
-            } else {
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                map.put(key, list);
+            if (!map.containsKey(key)) {   // not containKeys
+                map.put(key, new ArrayList<>());
             }
+            map.get(key).add(s);
         }
 
         return new ArrayList<>(map.values());
