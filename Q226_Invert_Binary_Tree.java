11eaa53e3aeed619d1aa861c3f03d7ea0e0491ee
Karan Suthar <kan.suthar666@gmail.com>
2024-07-03 17:59:53 +0530

    solved leetcode problem 226

---
+++ Q226_Invert_Binary_Tree.java
@@ -0,0 +1,15 @@
+public class Q226_Invert_Binary_Tree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode right = root.right;
+        root.right = root.left;
+        root.left = right;
+
+        return root;
+    }
+    
+}
