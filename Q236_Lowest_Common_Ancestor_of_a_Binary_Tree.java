6c75e8ca9a8d69d0d1eefe78c34e88c3144c059f
Karan Suthar <kan.suthar666@gmail.com>
2024-07-03 20:30:04 +0530

    solved leetcode problem 236

---
+++ Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,32 @@
+public class Q236_Lowest_Common_Ancestor_of_a_Binary_Tree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+
+        if (root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+
+        if (left == null)
+            return right;
+        else
+            return left;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
